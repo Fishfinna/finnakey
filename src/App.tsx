8230a7618ca3990cde92ba4dfe7c08e5e19b324d
Shanti <fishfinna12@gmail.com>
2024-01-28 16:10:53 -0800

    footer and header

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Error from "./pages/Error/Error";
 
+// components
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
 export default function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </>
   );
 }
